4dddff8e891e19bc4df3eb183162988c76143192
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-07-07 15:17:09 +0800

    docs

---
+++ docs/conf.py
@@ -0,0 +1,58 @@
+import os
+import sys
+from importlib.metadata import distribution
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'FixMS'
+copyright = '2023, Alec Thomson'
+author = 'Alec Thomson'
+release = distribution("fixms").version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.inheritance_diagram",
+    "myst_parser",
+    "autoapi.extension",
+]
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+]
+source_suffix = [".rst"]
+
+napoleon_google_docstring = True
+napoleon_use_param = False
+napoleon_use_ivar = True
+
+autoapi_type = "python"
+autoapi_dirs = ["../fixms"]
+autoapi_member_order = "groupwise"
+autoapi_keep_files = False
+autoapi_root = "autoapi"
+autoapi_add_toctree_entry = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
