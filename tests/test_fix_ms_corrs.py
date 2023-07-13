96c66c123f4706b0df1b358abbde90b6b81891a1
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-07-13 09:58:43 +0800

    Add test

---
+++ tests/test_fix_ms_corrs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test the fixing the MS correlations
+"""
+import pytest
+from fixms.fix_ms_corrs import get_pol_axis, convert_correlations, fix_ms_corrs
+
+class Tester:
+    pass
